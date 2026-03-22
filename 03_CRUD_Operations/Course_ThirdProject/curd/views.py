8b49e4ab70986cf756d5f80a4279fecbc9f92a2d
ZuevAL <zueve2007@gmail.com>
2026-03-22 11:45:46 +0300

    Add files via upload

---
+++ 03_CRUD_Operations/Course_ThirdProject/curd/views.py
@@ -0,0 +1,45 @@
+from django.forms.models import model_to_dict
+from django.shortcuts import render
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from .models import Person
+from .forms import PersonForm
+
+
+def index(request):
+    form = PersonForm()
+    people = Person.objects.all()
+    return render(request, 'index.html', {'form': form, 'people': people})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return HttpResponseRedirect('/')
+
+
+def edit(request, id):
+    try:
+        person = Person.objects.get(id=id)
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Person not found</h2>')
+
+    if request.method == 'POST':
+        form = PersonForm(request.POST, instance=person)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = PersonForm(model_to_dict(person))
+        return render(request, 'edit.html', {'form': form})
+
+
+def delete(request, id):
+    try:
+        person = Person.objects.get(id=id)
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Person not found</h2>')
+
+    person.delete()
+    return HttpResponseRedirect('/')
